1cb36a7acfac095db3c1fac1e32c95fab30a0ff3
Code.Jay <jasonjapheth265@gmail.com>
2026-01-05 11:39:34 -0800

    Update promoProducts.js

--- promoProducts.js
+++ promoProducts.js
@@ -2,22 +2,23 @@ module.exports = [
   {
     id: 1,
     name: "Lattafa Asad (EDP)",
-    price: 39999,
+    price: 33149,
     image: "/images/asad.png"
   },
   {
     id: 2,
     name: "Lattafa Khamrah",
-    price: 38999,
+    price: 40799,
     image: "/images/Asad2.png"
   },
   {
     id: 3,
     name: "Jean Lowe Immortel",
-    price: 41999,
+    price: 50149,
     image: "/images/jean_lowe.png"
   }
 ];
+
 
 
 
