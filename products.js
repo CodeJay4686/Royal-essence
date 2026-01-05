4ca50021301f794f23e1f5d90f4c165b79160fdc
Code.Jay <jasonjapheth265@gmail.com>
2026-01-05 03:59:25 -0800

    Update products.js

--- products.js
+++ products.js
@@ -1,26 +1,74 @@
 module.exports = [
   {
     id: 1,
-    name: "Baccarat Rouge 540",
-    price: 50000,
-    image: "/images/br540.png"
+    name: "Lattafa Asad (EDP)",
+    price: 100,
+    image: "/images/asad.png"
   },
   {
     id: 2,
-    name: "Dior Sauvage Elixir",
+    name: "Lattafa Khamrah",
     price: 45000,
-    image: "/images/sauvage.png"
+    image: "/images/Asad2.png"
   },
   {
     id: 3,
-    name: "Tom Ford Oud Wood",
+    name: "Jean Lowe Immortel",
     price: 48000,
-    image: "/images/oudwood.png"
+    image: "/images/jean_lowe.png"
   },
   {
     id: 4,
-    name: "YSL Y EDP",
+    name: "Armaf Club de Nuit Intense Man",
     price: 42000,
-    image: "/images/ysl.png"
+    image: "/images/Armaf-club-de-nuit-intense-man-eau-de-toilette-for-men.jpg"
+  },
+  {
+    id: 5,
+    name: "Afnan 9PM",
+    price: 61999,
+    image: "/images/afnan_9pm.png"
+  },
+  {
+    id: 6,
+    name: "Lattafa Badee Al Oud – Oud for Glory",
+    price: 36999,
+    image: "/images/Lattafa-Badee-Al-Oud-Oud-for-Glory-EDP.jpg"
+  },
+  {
+    id: 7,
+    name: "Lattafa His Confession EDP 100ml",
+    price: 48999,
+    image: "/images/his_confession.png"
+  },
+  {
+    id: 8,
+    name: "Afnan Supremacy Collector's Edition",
+    price: 89999,
+    image: "/images/supremacy.png"
+  },
+  {
+    id: 9,
+    name: "Ramz Silver by Lattafa EDP",
+    price: 43999,
+    image: "/images/ramz.png"
+  },
+  {
+    id: 10,
+    name: "Rifaaqat EDP – Emir Series",
+    price: 54999,
+    image: "/images/rifaaquat.png"
+  },
+  {
+    id: 11,
+    name: "Pride Eternal Oud – Arabian Perfume",
+    price: 53999,
+    image: "/images/eternal_aud.png"
+  },
+  {
+    id: 12,
+    name: "Lattafa Pride Vintage Radio EDP 100ml",
+    price: 64999,
+    image: "/images/vintage _radio.png"
   }
 ];
