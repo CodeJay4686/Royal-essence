0d54954237b809f2ea93ef62f5020ab596479e50
Code.Jay <jasonjapheth265@gmail.com>
2026-01-05 06:43:16 -0800

    Add files via upload

---
+++ promoProducts.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Lattafa Asad (EDP)",
+    price: 39999,
+    image: "/images/asad.png"
+  },
+  {
+    id: 2,
+    name: "Lattafa Khamrah",
+    price: 38999,
+    image: "/images/Asad2.png"
+  },
+  {
+    id: 3,
+    name: "Jean Lowe Immortel",
+    price: 41999,
+    image: "/images/jean_lowe.png"
+  }
+];
